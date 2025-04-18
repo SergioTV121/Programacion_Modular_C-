7d83e422c17ea270b372b3fdc49bfbbdeab33a01
SergioTV121 <sergiotv121@gmail.com>
2025-04-18 00:38:41 -0600

    Add files via upload

---
+++ Practica 1/Ej1/Ej1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace writteANDread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+     
+            Console.WriteLine("MI NOMBRE ES SERGIO ERNESTO Y MIS APELLIDOS ");
+            Console.Write("TINOCO");
+            Console.Write("VIDEGARAY");
+            Console.WriteLine("Y TENGO:  ");
+            Console.Write("16  AÑOS");
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ Practica 1/Ej2/Ej2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace writteANDread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Cecyt 8 Narsiso Bassols  ");
+            Console.WriteLine("U.A Programacion Modular  ");
+            Console.WriteLine("MI NOMBRE ES SERGIO ERNESTO Y MIS APELLIDOS ");
+            Console.Write("TINOCO ");
+            Console.Write("VIDEGARAY ");
+            Console.WriteLine(" Y TENGO:  ");
+            Console.WriteLine(" 16 AÑOS ");
+            Console.WriteLine("Mi Boleta es 2017089821");
+            Console.WriteLine("Mi grupo es el 4IV13");
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
