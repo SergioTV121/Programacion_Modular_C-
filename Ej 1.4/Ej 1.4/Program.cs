bff39964e5f447d5b819e7bfc70f930bea38087c
SergioTV121 <sergiotv121@gmail.com>
2025-04-20 23:25:50 -0600

    Add files via upload

---
+++ Ej 1.4/Ej 1.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Segundo
+{
+	
+    class Program
+    {
+    	
+        public static void Main(string[] args)
+        {
+Console.Title="Sergio Tinoco";
+
+double num;
+int cont=1;
+
+        	
+{
+         Console.WriteLine("Ingresa el numero:  ");
+      num=float.Parse(Console.ReadLine());
+      
+      
+      
+      
+      for(cont=1;cont<=10;cont++)
+      {
+      
+      
+           Console.WriteLine(" {0} X {1}= {2} ",num,cont,num*cont);   
+      
+      
+    
+      }
+      
+      
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
+}
