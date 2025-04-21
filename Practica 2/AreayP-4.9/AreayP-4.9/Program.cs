c66b7f3d698cd6a2e14768be2799ddbb2182c206
SergioTV121 <sergiotv121@gmail.com>
2025-04-20 23:22:02 -0600

    Add files via upload

---
+++ Practica 2/AreayP-4.9/AreayP-4.9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace areayaltura2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        	float b, h, area, perimetro;
+        	
+        	
+            Console.WriteLine("Ingresa la Base del rectangulo:  ");
+            b=float.Parse(Console.ReadLine());
+            
+            
+            Console.WriteLine("Ingresa la Altura del rectangulo:  ");
+            h=float.Parse(Console.ReadLine());
+            
+            area=b*h;
+            
+            perimetro=2*(b+h);
+            	
+            	
+            Console.WriteLine("El Area del rectuangulo es: {0:f2} y el perimetro es: {1:f2} ", area,perimetro);
+      
+            
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ Practica 2/conversiones 6.9/conversiones 6.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace areayaltura2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        	float cantconv, euros;
+        	
+        	Console.ForegroundColor=ConsoleColor.Cyan;
+        	      Console.BackgroundColor=ConsoleColor.Black;
+        	        Console.Clear ();
+        	
+        	
+
+      
+            
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
