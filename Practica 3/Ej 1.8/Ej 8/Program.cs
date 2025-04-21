d5f939cf67886bec98acc5ae65e2d9c99a080eef
SergioTV121 <sergiotv121@gmail.com>
2025-04-20 23:29:29 -0600

    Add files via upload

---
+++ Practica 3/Ej 1.8/Ej 8/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace Segundo
+{
+	
+	class Program
+	{
+    
+	
+    	
+	public static void Main(string[]args)
+        { 
+Console.Title= "Sergio Tinoco";
+
+
+
+
+char otro;
+int edad=0,acum=0,acum2=0;
+float mayor=0,menor=200;
+	{
+
+
+do{
+
+		
+      Console.WriteLine("Ingrese la Edad: ");
+      edad=int.Parse(Console.ReadLine());
+      
+    
+      
+      if(edad>mayor)
+      	mayor=edad;
+      
+ 
+ 
+      if(edad<menor)
+      {
+   
+      	menor=edad;
+
+
+      }
+      
+      
+      if(edad>=18)
+      {
+      	acum=acum+1;
+      }
+      else
+      	if(edad<18)
+      		acum2=acum2+1;
+      
+      
+      
+      Console.WriteLine(" ¿Otra Edad?  S/N ");
+      otro=char.Parse(Console.ReadLine());   
+
+      
+}while (otro=='S'||otro=='s');
+
+     
+      
+Console.WriteLine(" El numero de personas mayores de edad  es: {0} \n Y el de menores de edad es: {1} ",acum,acum2);
+            
+Console.WriteLine(" La Mayor edad Ingresada fue:  {0} \n Y la menor fue: {1} años",mayor,menor);
+
+
+            Console.ReadKey();
+
+		}
+        }
+	}
+}
---
+++ Practica 3/Ej 1.9/EJ 1.9/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace ej_4_ver_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Title="Sergio Tinoco";
+			
+			float din, cont=0;
+			string nombre;
+
+	do{
+	Console.WriteLine("Ingrese nombre del usuario:");
+	nombre=Console.ReadLine();
+	Console.WriteLine("Ingrese cantidad de dinero:");
+	din=float.Parse(Console.ReadLine());
+
+	if(din>0 || din<0)
+	{
+		if (din>0)
+		{
+			Console.WriteLine("Nombre: {0}\nSaldo: {1}\nEstado de la cuenta: Acreedor", nombre,din);
+			cont=cont+din;
+		}
+		else
+			Console.WriteLine("Nombre: {0}\nSaldo: {1}\nEstado de la cuenta: Deudor", nombre,din);
+	}
+	else
+			Console.WriteLine("Nombre: {0}\nSaldo: {1}\nEstado de la cuenta: Nulo", nombre,din);
+	Console.WriteLine ();
+}while(din>-1);
+			
+			Console.WriteLine("Sumatoria de saldos acreedores: {0}", cont);
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
