50d270113a57c93dd7f0e8588325101bf795b5d5
SergioTV121 <sergiotv121@gmail.com>
2025-04-20 23:21:25 -0600

    Add files via upload

---
+++ Practica 2/6.9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace areayaltura2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        	float cantconv, euros;
+        	
+        	Console.ForegroundColor=ConsoleColor.Cyan;
+        	      Console.BackgroundColor=ConsoleColor.Black;
+        	        Console.Clear ();
+        	
+        	
+            Console.WriteLine("Ingrese cantidad de marcos: ");
+            cantconv=float.Parse(Console.ReadLine());
+            
+            euros=(cantconv/12.2f)*1.09f;
+            	
+      
+            Console.WriteLine("{0:f2} marcos son {1:f2} euros",cantconv,euros);
+      
+            
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ Practica 2/CONVERSIONES.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace areayaltura2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        	float cantconv, euros;
+        	
+        	
+            Console.WriteLine("Ingrese cantidad de pesos: ");
+            cantconv=float.Parse(Console.ReadLine());
+            
+            euros=(cantconv/22.5f)*1.09f;
+            	
+            	
+            	
+            Console.WriteLine("{0:f2} pesos son {1:f2} euros",cantconv,euros);
+            
+            
+            //2do:
+            
+            Console.WriteLine("Ingrese cantidad de dolares: ");
+            cantconv=float.Parse(Console.ReadLine());
+            
+            euros=cantconv*1.09f;
+            	
+      
+            Console.WriteLine("{0:f2} dolares son {1:f2} euros",cantconv,euros);           
+            
+            
+            
+            //3ro:
+            
+            
+            Console.WriteLine("Ingrese cantidad de marcos: ");
+            cantconv=float.Parse(Console.ReadLine());
+            
+            euros=(cantconv/12.2f)*1.09f;
+            	
+      
+            Console.WriteLine("{0:f2} marcos son {1:f2} euros",cantconv,euros);           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+      
+            
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
